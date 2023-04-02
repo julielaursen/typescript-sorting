727f7517c567c18f4abd74616cc52acc5493ea1b
julielaursen <julielaursen1@gmail.com>
2023-04-02 00:02:01 -0500

    stopping at video 90

--- build/index.js
+++ build/index.js
@@ -1,22 +1,26 @@
 "use strict";
-class Sorter {
-    constructor(collection) {
-        this.collection = collection;
-    }
-    sort() {
-        const { length } = this.collection;
-        // video 83
-        for (let i = 0; i < length; i++) {
-            for (let j = 0; j < length - i - 1; j++) {
-                if (this.collection[j] > this.collection[j + 1]) {
-                    const leftSide = this.collection[j];
-                    this.collection[j] = this.collection[j + 1];
-                    this.collection[j + 1] = leftSide;
-                }
-            }
-        }
-    }
-}
-const sorter = new Sorter([10, 3, -5, 0]);
-sorter.sort();
-console.log(sorter.collection);
+// class Sorter {
+//   constructor(public collection: number[] | string) {}
+//   sort(): void {
+//     const { length } = this.collection
+//     // video 83 on bubble sort
+//     for (let i = 0; i < length; i++) {
+//       for (let j = 0; j < length - i - 1; j++) {
+//         // typeguard, collection === number[]
+//         if (this.collection instanceof Array) {
+//           if (this.collection[j] > this.collection[j + 1]) {
+//             const leftSide = this.collection[j]
+//             this.collection[j] = this.collection[j + 1]
+//             this.collection[j + 1] = leftSide
+//           }
+//         }
+//         if (typeof this.collection === 'string') {
+//         // typeguard, collection === string
+//         }
+//       }
+//     }
+//   }
+// }
+// const sorter = new Sorter([10, 3, -5, 0])
+// sorter.sort()
+// console.log(sorter.collection)
--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,27 @@
-class Sorter {
-  constructor(public collection: number[]) {}
+// class Sorter {
+//   constructor(public collection: number[] | string) {}
 
-  sort(): void {
-    const { length } = this.collection
-    // video 83
+//   sort(): void {
+//     const { length } = this.collection
+//     // video 83 on bubble sort
 
-    for (let i = 0; i < length; i++) {
-      for (let j = 0; j < length - i - 1; j++) {
-        if (this.collection[j] > this.collection[j + 1]) {
-          const leftSide = this.collection[j];
-          this.collection[j] = this.collection[j + 1];
-          this.collection[j + 1] = leftSide;
-        }
-      }
-    }
-  }
-}
-const sorter = new Sorter([10, 3, -5, 0]);
-sorter.sort();
-console.log(sorter.collection);
+//     for (let i = 0; i < length; i++) {
+//       for (let j = 0; j < length - i - 1; j++) {
+//         // typeguard, collection === number[]
+//         if (this.collection instanceof Array) {
+//           if (this.collection[j] > this.collection[j + 1]) {
+//             const leftSide = this.collection[j]
+//             this.collection[j] = this.collection[j + 1]
+//             this.collection[j + 1] = leftSide
+//           }
+//         }
+//         if (typeof this.collection === 'string') {
+//         // typeguard, collection === string
+//         }
+//       }
+//     }
+//   }
+// }
+// const sorter = new Sorter([10, 3, -5, 0])
+// sorter.sort()
+// console.log(sorter.collection)
