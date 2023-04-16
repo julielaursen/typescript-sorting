d99b1aadb272d673f4fcc330ffa96d993fe26a72
julielaursen <julielaursen1@gmail.com>
2023-04-15 19:18:51 -0500

    update characters collection

--- src/Sorter.ts
+++ src/Sorter.ts
@@ -1,6 +1,12 @@
 import { NumbersCollection  } from './NumbersCollection';
+
+interface Sortable {
+  length: number;
+  compare(leftIndex: number, rightIndex: number): boolean;
+  swap(leftIndex: number, rightIndex: number): void;
+}
 export class Sorter {
-    constructor(public collection: NumbersCollection) {}
+    constructor(public collection: Sortable) {}
   
     sort(): void {
       const { length } = this.collection
@@ -8,17 +14,12 @@ export class Sorter {
   
       for (let i = 0; i < length; i++) {
         for (let j = 0; j < length - i - 1; j++) {
-            if (this.collection[j] > this.collection[j + 1]) {
-              const leftSide = this.collection[j];
-              this.collection[j] = this.collection[j + 1];
-              this.collection[j + 1] = leftSide;
+            if (this.collection.compare(j, j + 1)){
+              this.collection.swap(j, j + 1);
             }
           }
         }
       }
     }
   
-  const sorter = new Sorter([10, 3, -5, 0])
-  sorter.sort()
-  console.log(sorter.collection)
   
